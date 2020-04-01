95518d1b7aff9f03d4d2f173127297e0b175df1c
Chr1sVanTun <christoph.dahlitz@web.de>
2020-04-01 11:28:13 +0200

    Updating messages

--- reminder.py
+++ reminder.py
@@ -4,11 +4,11 @@ import ctypes
 MB_SETFOREGROUND = 0x00010000
 MBeep = 0x00000000
 
-print("How many sets you want to do?")
+print("How many sets you want to perform?")
 amount = input()
 print("How many minutes you want to rest between the sets?")
 minutes = float(input())
-print("Do your first Set, you will be reminded for the next " + str(amount) + " sets")
+print("Do your first Set, you will be remembered for the next " + str(amount) + " sets")
 
 for x in range(0, int(amount)):
     local_time = float(minutes*60)
