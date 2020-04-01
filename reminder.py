340afb71a579f6c0d7994d85f6ba8740c03cddc1
Chr1sVanTun <christoph.dahlitz@web.de>
2020-04-01 11:24:56 +0200

    Initial commit

---
+++ reminder.py
@@ -0,0 +1,18 @@
+import time
+import ctypes
+
+MB_SETFOREGROUND = 0x00010000
+MBeep = 0x00000000
+
+print("How many sets you want to do?")
+amount = input()
+print("How many minutes you want to rest between the sets?")
+minutes = float(input())
+print("Do your first Set, you will be reminded for the next " + str(amount) + " sets")
+
+for x in range(0, int(amount)):
+    local_time = float(minutes*60)
+    time.sleep(local_time)
+    MessageBeep = ctypes.windll.user32.MessageBeep(MBeep)
+    MessageBox = ctypes.windll.user32.MessageBoxW
+    MessageBox(None, 'Do 21 Air Squats, followed by 12 Push-Ups!!', 'Time for Training!', MB_SETFOREGROUND, 0)
